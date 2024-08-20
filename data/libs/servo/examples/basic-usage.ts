3c1d95757a055d626cab52605cadef719e728979
C2Coder <C2Coder@users.noreply.github.com>
2024-08-20 23:08:00 +0000

    deploy: 7a0dc544abfb720b09265c035e59dc51f214091d

---
+++ data/libs/servo/examples/basic-usage.ts
@@ -0,0 +1,10 @@
+import { Servo } from "./libs/servo.js"
+
+const SERVO_PIN = 35;
+const TIMER = 1;
+const CHANNEL = 3;
+const servo = new Servo(SERVO_PIN, TIMER, CHANNEL);
+
+servo.write(0);    // 0°
+servo.write(512);  // 90°
+servo.write(1023); // 180°
