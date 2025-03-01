8bac0821da391d0b98dfcce8df3c76c61dc5c816
C2Coder <C2Coder@users.noreply.github.com>
2025-03-01 10:20:19 +0000

    deploy: dcabdbdb595b2d376eb96321dcb5d0bb2f7a99a0

--- data/robutek/robutek.js
+++ data/robutek/robutek.js
@@ -2,7 +2,7 @@ import * as adc from "adc";
 import * as gpio from "gpio";
 import * as motor from "motor";
 import * as ledc from "ledc";
-const robutekDiameter = 80; // mm
+const robutekDiameter = 83; // mm
 const wheelDiameter = 33.3; // mm
 const wheelCircumference = Math.PI * wheelDiameter;
 export var PenPos;
@@ -84,8 +84,18 @@ const leftMotorPins = { motA: Pins.Motor1A, motB: Pins.Motor1B, encA: Pins.Enc1A
 const rightMotorPins = { motA: Pins.Motor2A, motB: Pins.Motor2B, encA: Pins.Enc2A, encB: Pins.Enc2B };
 const leftMotorLedc = { timer: 0, channelA: 0, channelB: 1 };
 const rightMotorLedc = { timer: 0, channelA: 2, channelB: 3 };
-export const leftMotor = new motor.Motor({ pins: leftMotorPins, ledc: leftMotorLedc, encTicks: 410, circumference: wheelCircumference });
-export const rightMotor = new motor.Motor({ pins: rightMotorPins, ledc: rightMotorLedc, encTicks: 410, circumference: wheelCircumference });
+const reg = {
+    kp: 7000,
+    ki: 350,
+    kd: 350,
+    kv: 166,
+    ka: 11000,
+    kc: 72,
+    maxIOut: 1023,
+    unwindFactor: 1
+};
+export const leftMotor = new motor.Motor({ pins: leftMotorPins, ledc: leftMotorLedc, encTicks: 812, reg, circumference: wheelCircumference });
+export const rightMotor = new motor.Motor({ pins: rightMotorPins, ledc: rightMotorLedc, encTicks: 812, reg, circumference: wheelCircumference });
 adc.configure(Pins.Sens1, adc.Attenuation.Db0);
 adc.configure(Pins.Sens2, adc.Attenuation.Db0);
 adc.configure(Pins.Sens3, adc.Attenuation.Db0);
@@ -94,11 +104,18 @@ gpio.pinMode(Pins.SensEN, gpio.PinMode.OUTPUT);
 gpio.write(Pins.SensEN, 1);
 gpio.pinMode(Pins.SensSW, gpio.PinMode.OUTPUT);
 let speed = 0;
+let ramp = 0;
 export function setSpeed(value) {
     speed = value;
 }
+export function setRamp(value) {
+    ramp = value;
+}
 export function getSpeed() {
     return speed;
+}
+export function getRamp() {
+    return ramp;
 }
 /**
  * Move the robot
@@ -122,6 +139,8 @@ export async function move(curve, duration) {
     }
     leftMotor.setSpeed(lMot * speed);
     rightMotor.setSpeed(rMot * speed);
+    leftMotor.setRamp(lMot * ramp);
+    rightMotor.setRamp(rMot * ramp);
     const hasTime = duration && duration.hasOwnProperty("time");
     const hasDistance = duration && duration.hasOwnProperty("distance");
     if (duration && (hasTime || hasDistance)) {
@@ -148,34 +167,28 @@ export async function move(curve, duration) {
 }
 /**
  * Rotate the robot
- * @param angle in degrees (optional)
+ * @param angle in degrees
  */
 export async function rotate(angle) {
     leftMotor.setSpeed(speed);
     rightMotor.setSpeed(speed);
-    if (!angle) {
-        await Promise.all([
-            leftMotor.move(),
-            rightMotor.move()
-        ]);
+    leftMotor.setRamp(ramp);
+    rightMotor.setRamp(ramp);
+    const arcLength = (Math.abs(angle) / 360) * Math.PI * robutekDiameter;
+    let lMot;
+    let rMot;
+    if (angle < 0) {
+        lMot = -arcLength;
+        rMot = arcLength;
     }
     else {
-        const arcLength = (Math.abs(angle) / 360) * Math.PI * robutekDiameter;
-        let lMot;
-        let rMot;
-        if (angle < 0) {
-            lMot = -arcLength;
-            rMot = arcLength;
-        }
-        else {
-            lMot = arcLength;
-            rMot = -arcLength;
-        }
-        await Promise.all([
-            leftMotor.move({ distance: lMot }),
-            rightMotor.move({ distance: rMot })
-        ]);
+        lMot = arcLength;
+        rMot = -arcLength;
     }
+    await Promise.all([
+        leftMotor.move({ distance: lMot }),
+        rightMotor.move({ distance: rMot })
+    ]);
 }
 /**
  * Stop the robot
